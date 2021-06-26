5b4c882513d9e4247f44ac89de244e8bd27cff9c
kallistahalim <kallistahalim@gmail.com>
2021-06-26 19:07:13 +0700

    add firebase because desperate

---
+++ js/submit-greetings.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDdW0cRiPL24NkbsKDAVaRZVQ1F-kTBU88",
+    authDomain: "paulus-nadia.firebaseapp.com",
+    projectId: "paulus-nadia",
+    storageBucket: "paulus-nadia.appspot.com",
+    messagingSenderId: "327859259590",
+    appId: "1:327859259590:web:059e5035aef92b2a012fc7",
+    measurementId: "G-LS0WLHMVDK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
